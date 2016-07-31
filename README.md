e7a756b9498cd8610d8b2272213a21d8f1e62af8
Israel Sotomayor <sotoisra24@gmail.com>
2016-07-31 10:16:23 +0200

    new: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Network Security Group Terraform module
+
+A Terraform module to provide a Network Security Group on Microsoft Azure.
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "id" {
+    value = "${azurerm_network_security_group.mod.id}"
+}
+
+output "name" {
+    value = "${azurerm_network_security_group.mod.name}"
+}
